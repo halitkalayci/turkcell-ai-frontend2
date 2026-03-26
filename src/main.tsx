f90275d05a659ae8912b82d6b747c5be2c4bb81c
halitklyc <halitenes.kalayci@gmail.com>
2026-03-26 13:39:15 +0300

    global styles uygulandı

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import './styles/variables.css'
+import './styles/reset.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
